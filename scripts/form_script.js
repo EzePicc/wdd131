4e4e669b32b17139b31c303d67dbaad5788aae77
EzePicc <epiccardobelen@byupathway.org>
2026-06-05 18:58:39 -0300

    done

--- scripts/form_script.js
+++ scripts/form_script.js
@@ -25,3 +25,20 @@ const products = [
     averagerating: 5.0
   }
 ];
+
+const productSelect = document.querySelector("#productName");
+
+products.forEach(product => {
+    
+    const option = document.createElement("option");
+    option.value = product.id;
+
+    option.textContent = product.name;
+
+    productSelect.appendChild(option);
+}
+);
+
+document.querySelector("#lastModified").textContent =
+
+`Last Modified: ${document.lastModified}`;
